0f8cf1c350e830627a0314ffb52f52ddf83bd9af
Marina Ferrer <ferrergarridom@gmail.com>
2023-04-25 01:35:57 +0200

    Updated layout

--- app/src/main/java/com/dam2/m09_tic_tac_toe/MainActivity.java
+++ app/src/main/java/com/dam2/m09_tic_tac_toe/MainActivity.java
@@ -1,14 +1,34 @@
 package com.dam2.m09_tic_tac_toe;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.Button;
+import com.google.android.material.textfield.TextInputEditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextInputEditText ip;
+    private TextInputEditText port;
+    private Button connect;
+
+    private Button start;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ip = findViewById(R.id.ipEditTextInput);
+        port = findViewById(R.id.portEditTextInput);
+        connect = findViewById(R.id.connectBtn);
+
+        connect.setOnClickListener(view -> {
+            // if connection ok
+
+        });
+
+        start = findViewById(R.id.startBtn);
+
+
     }
 }
